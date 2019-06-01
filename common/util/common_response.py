a3e52527930b1a64f97dfeb56a3057c2c5cadcb8
jayanth <jayanth@cartoonmango.com>
2019-06-01 11:54:06 +0530

    Cookie response method

--- common/util/common_response.py
+++ common/util/common_response.py
@@ -9,3 +9,10 @@ def response(message, data=None, status=200):
     if data is not None:
         response_obj['data'] = data
     return jsonify(response_obj), status
+
+
+def cookie_response(message, cookie_name, cookie_value, expires="100", data=None,
+                    status=200):
+    c_response_tuple = response(message=message, data=data, status=status)
+    c_response_tuple[0].set_cookie(cookie_name, cookie_value)
+    return c_response_tuple
